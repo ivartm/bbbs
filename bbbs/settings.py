c29ee2ad67db5fb69fdd7ed29adf19dd6676db89
Mike <mike94.0653@ya.ru>
2021-05-14 21:58:13 +0300

    start

--- bbbs/settings.py
+++ bbbs/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'users'
+
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,16 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+
--- users/models.py
+++ users/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.conf import settings
+# from common.models import City
 
-# Create your models here.
+
+class Profile(models.Model):
+    class Role(models.TextChoices):
+        MENTOR = 'mentor'
+        MODERATOR_REG = 'moderator_regional'
+        MODERATOR_GEN = 'moderator_general'
+        ADMIN = 'admin'
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # city = models.OneToOneField('City', on_delete=models.RESTRICT)
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+    )
+
+    def __str__(self):
+        return 'Профиль пользователя {}'.format(self.user.username)
+
+    @property
+    def is_admin(self):
+        return self.Role.ADMIN == self.role
+
+    @property
+    def is_moderator_reg(self):
+        return self.Role.MODERATOR_REG == self.role
+
+    @property
+    def is_moderator_gen(self):
+        return self.Role.MODERATOR_GEN == self.role
+
+    @property
+    def is_mentor(self):
+        return self.Role.MENTOR == self.role
