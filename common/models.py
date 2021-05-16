e6dbaf4e330cd2ba3983515b52b1f323dcfe5886
Konstantin Raikhert <konstantinraihert@yandex.ru>
2021-05-16 13:03:34 +0300

    add City model

--- common/models.py
+++ common/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.base import Model
 
-# Create your models here.
+User = get_user_model()
+
+
+class City(models.Model):
+    name = models.CharField(max_length=30)
+    is_primary = models.BooleanField(default=False)   # Noqa не совсем понятен параметр
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "City"
+        verbose_name_plural = "Cities"
