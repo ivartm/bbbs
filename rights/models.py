3cd927a4f2a84acb3080de16e5109f1eb0882348
nkiryanov <70921439+nkiryanov@users.noreply.github.com>
2021-06-12 18:11:28 +0300

    unnecessry import was deleted

--- rights/models.py
+++ rights/models.py
@@ -5,8 +5,6 @@ from django.utils.html import format_html
 
 from common.utils import slugify
 
-from common.utils import slugify
-
 
 class RightTag(models.Model):
     name = models.CharField(max_length=50, unique=True)
