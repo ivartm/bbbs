ffa97a05e0b5d3140270411ac657f3f147f7f1dc
nkiryanov <70921439+nkiryanov@users.noreply.github.com>
2021-06-18 04:57:24 +0300

    Ready for secrets and real depoly

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -8,12 +8,18 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS",
+    default=[".bbbs.ru", ".kiryanov.ru"],
+)
+
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.postgresql"),
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("POSTGRES_DB"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
@@ -21,10 +27,6 @@ DATABASES = {
         "PORT": os.environ.get("POSTGRES_PORT"),
     }
 }
-
-
-# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["*"]
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
 from drf_yasg import openapi
@@ -38,17 +37,17 @@ schema_view = get_schema_view(
 )
 
 urlpatterns += [
-    url(
+    path(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
     ),
-    url(
+    path(
         r"^swagger/$",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
-    url(
+    path(
         r"^redoc/$",
         schema_view.with_ui("redoc", cache_timeout=0),
         name="schema-redoc",
