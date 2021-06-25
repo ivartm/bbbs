fee726258464d9603cd26e17b33ebcc82f2d3698
Konstantin Raikhert <konstantinraihert@yandex.ru>
2021-06-25 22:05:17 +0300

    ordering all API

--- questions/views.py
+++ questions/views.py
@@ -12,22 +12,15 @@ class QuestionsList(ListCreateAPIView):
         Question.objects.exclude(answer__exact="")
         .all()
         .prefetch_related("tags")
+        .order_by("id")
     )
     serializer_class = QuestionSerializer
     permission_classes = [AllowAny]
     filter_backends = (DjangoFilterBackend,)
     filterset_class = QuestionFilter
 
-    # def get(self, request):
-    #     paths = {
-    #         '/api/v1/questions/': QuestionTag.objects.get,
-    #     }
-    #     for key, value in paths.items():
-    #         if self.path == key:
-    #             print(value(id=1))
-
 
 class QuestionsTagList(ListAPIView):
-    queryset = QuestionTag.objects.all().order_by("name")
+    queryset = QuestionTag.objects.all().order_by("id")
     serializer_class = QuestionTagSerializer
     permission_classes = [AllowAny]
