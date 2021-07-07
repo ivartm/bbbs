b831eb8744700c85bf36707bf9a79f8bd7595ab3
Анна Полякова <Sova040888@yandex.ru>
2021-07-07 15:33:27 +0300

    fix views meeting

--- common/views.py
+++ common/views.py
@@ -5,14 +5,11 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from petrovich.enums import Case
 from petrovich.main import Petrovich
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.generics import ListAPIView
 from rest_framework.mixins import (
-    CreateModelMixin,
-    DestroyModelMixin,
     ListModelMixin,
-    RetrieveModelMixin,
     UpdateModelMixin,
 )
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -47,13 +44,8 @@ class MyCityApiView(ListModelMixin, UpdateModelMixin, GenericViewSet):
         return queryset
 
 
-class MeetingAPIView(
-    ListModelMixin,
-    UpdateModelMixin,
-    CreateModelMixin,
-    GenericViewSet,
-    DestroyModelMixin,
-    RetrieveModelMixin,
+class MeetingViewSet(
+    viewsets.ModelViewSet,
 ):
     serializer_class = MeetingSerializer
     permission_classes = [IsOwner, IsAuthenticated]
@@ -82,28 +74,26 @@ class MeetingAPIView(
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-
-@api_view(["POST"])
-@permission_classes([IsAuthenticated, IsOwner])
-def send_meeting_to_curator(request):
-    serializer = MeetingMessageSerializer(data=request.data)
-    if serializer.is_valid():
-        meeting = get_object_or_404(
-            Meeting, id=request.data["id"], user=request.user
-        )
-        message = EmailMessage()
-        message.subject = (
-            f"Описание встречи: {meeting.place}, " f"{meeting.date}"
-        )
-        message.body = meeting.description
-        message.to = [meeting.user.profile.curator.email]
-        message.attach_file(meeting.image.path)
-        # message.template_id = SEND_MEETING_TEMPLATE_ID
-        message.send()
-        meeting.send_to_curator = True
-        meeting.save()
-        return JsonResponse({"success": True}, status=status.HTTP_200_OK)
-    else:
-        return JsonResponse(
-            {"success": False}, status=status.HTTP_400_BAD_REQUEST
-        )
+    @action(detail=False, methods=["post"], name="send_to_curator")
+    def send_to_curator(self, request):
+        serializer = MeetingMessageSerializer(data=request.data)
+        if serializer.is_valid():
+            meeting = get_object_or_404(
+                Meeting, id=request.data["id"], user=request.user
+            )
+            message = EmailMessage()
+            message.subject = (
+                f"Описание встречи: {meeting.place}, " f"{meeting.date}"
+            )
+            message.body = meeting.description
+            message.to = [meeting.user.profile.curator.email]
+            message.attach_file(meeting.image.path)
+            # message.template_id = SEND_MEETING_TEMPLATE_ID
+            message.send()
+            meeting.sendToCurator = True
+            meeting.save()
+            return JsonResponse({"success": True}, status=status.HTTP_200_OK)
+        else:
+            return JsonResponse(
+                {"success": False}, status=status.HTTP_400_BAD_REQUEST
+            )
