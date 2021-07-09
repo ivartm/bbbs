4a0865f1f78fb5c7a3913eaea68ada8a43f1d4b6
Анна Полякова <Sova040888@yandex.ru>
2021-07-10 01:56:46 +0300

    add validator to Meeting

--- common/serializers.py
+++ common/serializers.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from petrovich.enums import Case
+from petrovich.main import Petrovich
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
@@ -21,17 +23,34 @@ class MyCitySerializer(ModelSerializer):
 
 
 class MeetingSerializer(ModelSerializer):
-    name = serializers.CharField(read_only=True, max_length=100)
+    name = serializers.SerializerMethodField()
     place = serializers.CharField(max_length=50, min_length=2)
     description = serializers.CharField(max_length=5000, min_length=2)
     date = serializers.DateField()
 
     def validate(self, data):
-        if data["date"] > datetime.now().date():
-            raise serializers.ValidationError(
-                "date can not be more than today"
-            )
+        if "date" in data:
+            if data["date"] > datetime.now().date():
+                raise serializers.ValidationError(
+                    "date can not be more than today"
+                )
+            return data
         return data
+
+    def get_name(self, obj):
+        if obj.user.profile.curator is None:
+            return None
+        change_ending = Petrovich()
+        return (
+            change_ending.firstname(
+                value=obj.user.profile.curator.first_name,
+                case=Case.DATIVE,
+                # gender=user.profile.curator.gender,
+            )
+            + " "
+            + obj.user.profile.curator.last_name[0]
+            + "."
+        )
 
     class Meta:
         model = Meeting
